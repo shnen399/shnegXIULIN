b0fa6ca23e31698d518e351ebcebe2b8927a2ea6
shnen399 <t0970313177@gmail.com>
2025-08-08 16:46:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
@@ -7,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return JSONResponse(content={"message": "PIXNET 自動發文系統已啟動"})
+
+@app.post("/post_article")
+def post_article():
+    return JSONResponse(content={"message": "文章已成功發佈！"})
